2ee01f1547c3994f92483139017b1f08a8116e52
KrisTovski <kfilak@onet.eu>
2020-05-02 16:35:07 +0200

    Added Optional for CategoryRepository and UnitOfMeasureRepository

--- src/main/java/com/kristovski/spring5recipeapp/repositories/CategoryRepository.java
+++ src/main/java/com/kristovski/spring5recipeapp/repositories/CategoryRepository.java
@@ -3,5 +3,9 @@ package com.kristovski.spring5recipeapp.repositories;
 import com.kristovski.spring5recipeapp.model.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
 }
