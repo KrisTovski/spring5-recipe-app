a28427cd16695fe4065917b6d3be853f2bd63fc0
Kris <mesjashek@gmail.com>
2020-05-14 20:33:29 +0200

    Spring Integration test - failed

---
+++ src/test/java/com/kristovski/spring5recipeapp/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,41 @@
+package com.kristovski.spring5recipeapp.repositories;
+
+import com.kristovski.spring5recipeapp.model.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataJdbcTest
+public class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    public void setUp() throws Exception{
+    }
+
+    @Test
+    public void findByDescription() throws Exception{
+
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", uomOptional.get().getDescription());
+    }
+
+    @Test
+    public void findByDescriptionCup() throws Exception{
+
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+
+        assertEquals("Cup", uomOptional.get().getDescription());
+    }
+}
