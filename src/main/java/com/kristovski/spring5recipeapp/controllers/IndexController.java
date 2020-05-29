884a7847814e2f034a8e7dc7cb5990b5c0e13f51
Kris <mesjashek@gmail.com>
2020-05-29 18:02:30 +0200

    fixing thymeleaf/bootstrap

--- src/main/java/com/kristovski/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/com/kristovski/spring5recipeapp/controllers/IndexController.java
@@ -19,6 +19,7 @@ public class IndexController {
     @RequestMapping({"","/","/index"})
     public String getIndexPage(Model model){
         log.debug("Getting Index page");
+
         model.addAttribute("recipes", recipeService.getRecipies());
 
         return "index";
