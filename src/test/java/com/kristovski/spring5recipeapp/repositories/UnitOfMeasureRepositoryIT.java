1e919bdd7c18c4072235ae0421a1ff8882f8e51a
KrisTovski <kfilak@onet.eu>
2020-06-25 21:29:27 +0200

    added enumeration dropdowns (difficulty enum list)

--- src/test/java/com/kristovski/spring5recipeapp/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/com/kristovski/spring5recipeapp/repositories/UnitOfMeasureRepositoryIT.java
@@ -1,30 +1,31 @@
 package com.kristovski.spring5recipeapp.repositories;
 
 import com.kristovski.spring5recipeapp.model.UnitOfMeasure;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
+
 
 @RunWith(SpringRunner.class)
-@DataJdbcTest
+@DataJpaTest
 public class UnitOfMeasureRepositoryIT {
 
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
 
-    @BeforeEach
-    public void setUp() throws Exception{
+    @Before
+    public void setUp() throws Exception {
     }
 
     @Test
-    public void findByDescription() throws Exception{
+    public void findByDescription() throws Exception {
 
         Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
 
@@ -32,10 +33,11 @@ public class UnitOfMeasureRepositoryIT {
     }
 
     @Test
-    public void findByDescriptionCup() throws Exception{
+    public void findByDescriptionCup() throws Exception {
 
         Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
 
         assertEquals("Cup", uomOptional.get().getDescription());
     }
+
 }
